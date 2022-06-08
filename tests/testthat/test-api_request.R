a5b3805e56fb97603d8a7e1b38dae144a48d4f8b
Wang <wangq13@L02221555.niaid.nih.gov>
2022-06-08 12:03:50 -0400

    fix checking internet problem

--- tests/testthat/test-api_request.R
+++ tests/testthat/test-api_request.R
@@ -1,15 +1,18 @@
 test_that("api request works", {
 
-  INTERNET <- TRUE
+  #INTERNET <- TRUE
+  INTERNET <- check_internet()
 
   if (INTERNET) {
     TOKEN <- get_token()
     #TOKEN <- secret_make_key()
   }else{
-    TOKEN <- "Invalid credentials"
+    #TOKEN <- "Invalid credentials"
+    TOKEN <- NA
   }
 
-  if (TOKEN != "Invalid credentials" & INTERNET) {
+  #if (TOKEN != "Invalid credentials" & INTERNET) {
+  if (!is.na(TOKEN) & INTERNET) {
     r <- tidy_depot_api(path = "Biochemistry", token = TOKEN)
     expect_equal(S3Class(r), "tidy_depot_api")
     expect_equal(S3Class(r$content), "data.frame")
